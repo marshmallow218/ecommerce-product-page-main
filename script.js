6f5bf16f4636dc7320f4f5b0b3a5bd236c402d38
Kurt Renzo Jalalain <jalalainkurt@gmail.com>
2023-10-16 02:02:57 +0800

    added thumbnail style switch

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-/*** INITIALIZATION ***/
-const plus = document.querySelector('#plus')
-const minus = document.querySelector('#minus')
-const quantity = document.querySelector('.quantity')
-
-/*** QUANTITY ***/
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const quantity = document.querySelector('.quantity');
+const thumbnails = document.querySelectorAll('.product-thumbnail img');
 let a = 0;
-plus.addEventListener('click', () => {
+
+
+/*** CHANGE QUANTITY ***/
+function increase() {
     a++;
     quantity.innerText = a;
-})
-minus.addEventListener('click', () => {
-    a > 0 ? a--: a == a;
+}
+function decrease() {
+    a > 0 ? a--: a == 0;
     quantity.innerText = a;
+}
+
+/*** THUMBNAIL STYLE SWITCH ***/
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        document.querySelector('.selected')?.classList.remove('selected');
+        thumbnail.classList.add('selected');
+    })
 })
+
+
+plus.addEventListener('click', increase)
+minus.addEventListener('click', decrease)
